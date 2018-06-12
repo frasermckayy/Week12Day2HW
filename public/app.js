e2b2a497e10f6e89e1a64205d5bf75c0850fb418
Fraser McKay <frasermckay97@gmail.com>
2018-06-12 20:37:35 +0100

    changed location and places marker on map

--- public/app.js
+++ public/app.js
@@ -2,10 +2,11 @@ const drawMap = function() {
   const mapDiv = document.getElementById("main-map");
   const glasgow = [55.86515, -4.25763];
   const javaStreet = [55.801507, -4.003805];
-  const Rio_De_Janeiro = [22.9068, -43.1729];
+  const Rio_De_Janeiro = [-22.970722, -43.1729];
   const zoomlevel = 15;
   const mainMap = new MapWrapper(mapDiv, Rio_De_Janeiro, zoomlevel);
-console.log(Rio_De_Janeiro);
+  mainMap.addMarker(Rio_De_Janeiro);
+
 
   var button = document.querySelector('button');
    button.addEventListener('click', handleButtonClick);
